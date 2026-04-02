36d25d769674c7de9b6f76c848f7b001acb8bc88
rowyntheanimator <spauldingrowyn@gmail.com>
2026-04-01 22:24:05 -0400

    Refactor Main.cpp to use SD card for memory management

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <EEPROM.h>
+#include <SD.h>
 
 int A = 0;
 int B = 0;
@@ -6,10 +7,10 @@ int C = 0;
 int M = 0;
 int N = 0;
 int Z = 0;
-int PC = 0;
+uint16_t PC = 0;
 unsigned char MEM[65536] = {0};
-unsigned char PROGMEM[65536] = {0};
 unsigned char PERM_MEM[65536] = {0};
+unsigned char PROG_MEM[65536] = {0};
 int STACK[255] = {0};
 int sp = 0;
 int RUNNING = 1;
@@ -20,10 +21,6 @@ int b2 = 0;
 int b3 = 0;
 int b4 = 0;
 int b5 = 0;
-
-void hlt() {
-  RUNNING = 0;
-}
 
 void pua(int val_in) {
   A = val_in;
@@ -61,19 +58,19 @@ void dec() {
 }
 
 int stack_size() {
-    for (int i = 0; i < 255; i++) {
-        if (STACK[i] == 0) {
-        return i;
-        }
+  for (int i = 0; i < 255; i++) {
+    if (STACK[i] == 0) {
+      return i;
     }
-    return 255;
+  }
+  return 255;
 }
 
 void stack_append(int val) {
     if (sp < 255) {
         STACK[sp++] = val;
     } else {
-        printf("STACK OVERFLOW\n");
+        Serial.println("STACK OVERFLOW");
         while (1==1) {}
     }
 }
@@ -87,28 +84,28 @@ int parse_operand(int type, int operand, int operandb2) {
     int ex = 0;
     ex = (operand << 8) | operandb2;
     switch (type) {
-        case 0:
-            return operand;
-        case 1:
-            if (M == 0) {
-                return MEM[ex];
-            } else if (M == 1) {
-                return PERM_MEM[ex];
-            }
-        case 2:
-            return ex;
-        case 3:
-            return A;
-        case 4:
-            return B;
-        case 5:
-            return C;
-        case 6:
-            return M;
+      case 0:
+          return operand;
+      case 1:
+          if (M == 0) {
+              return MEM[ex];
+          } else if (M == 1) {
+            return PERM_MEM[ex];
+          }
+      case 2:
+          return ex;
+      case 3:
+          return A;
+      case 4:
+          return B;
+      case 5:
+          return C;
+      case 6:
+          return M;
     }
 
-    printf("Invalid operand: ");
-    printf("%d\n", operand);
+    Serial.print("Invalid operand: ");
+    Serial.println(operand);
     while (1==1) {}
 }
 
@@ -120,8 +117,8 @@ void sub(int valA, int valB) {
 }
 void DIV(int valA, int valB) {
     if (valB == 0) {
-        printf("DIVISION BY ZERO AT LINE ");
-        printf("%d\n", PC);
+      Serial.print("DIVISION BY ZERO AT LINE ");
+      Serial.println(PC);
     }
     N = valA / valB;
 }
@@ -135,78 +132,24 @@ void cmb(int val) {
         Z = 0;
     }
 }
-
-void load_PROGMEM() {
-    FILE *f = fopen("PROGMEM.bin", "rb");
-    if (!f) {
-        printf("ERROR OPENING PROGMEM.bin\n");
-        return;
-    }
-
-    fread(PROGMEM, 1, 65536, f);
-    fclose(f);
-}
-
-void load_PERM_MEM() {
-    FILE *f = fopen("PERM_MEM.bin", "rb");
-    if (!f) {
-        printf("ERROR OPENING PERM_MEM.bin\n");
-        return;
-    }
-
-    fread(PERM_MEM, 1, 65536, f);
-    fclose(f);
-}
-
-void write_PERM_MEM(int inx, char by) {
-    FILE *f = fopen("PERM_MEM.bin", "rb+");
-    if (!f) {
-        printf("ERROR OPENING PERM_MEM.bin\n");
-        return;
-    }
-
-    fseek(f, inx, SEEK_SET);
-    fwrite(&by, 1, 1, f);
-
-    fclose(f);
-}
-
-void write_PROGMEM(int inx, char by) {
-    FILE *f = fopen("PROGMEM.bin", "rb+");
-    if (!f) {
-        printf("ERROR OPENING PROGMEM.bin\n");
-        return;
-    }
-
-    fseek(f, inx, SEEK_SET);
-    fwrite(&by, 1, 1, f);
-
-    fclose(f);
-}
-
-void mem(int valA, int valB) {
+void mem(uint8_t valA, uint16_t valB) {
     valA = valA % 256;
     switch (M) {
-        case 0:
-            MEM[valB] = valA;
-            break;
-        case 1:
-            PERM_MEM[valB] = valA;
-            write_PERM_MEM(valB, valA);
-            break;
-        case 2:
-            PROGMEM[valB] = valA;
-            write_PROGMEM(valB, valA);
-            break;
-        default:
-            printf("ERROR: INVALD VALUE %d FOR M REGISTER", M);
+      case 0:
+        MEM[valB] = valA;
+      case 1:
+        PERM_MEM[valB] = valA;
+        savePERMEM(valB, valA);
+      case 2:
+        PROG_MEM[valB] = valA;
+        savePROGMEM(valB, valA);
     }
 }
 int stack_pop() {
     if (sp > 0) {
         return STACK[--sp];
     } else {
-        printf("STACK OVERFLOW");
+        Serial.println("STACK UNDERFLOW");
         while (1==1) {}
     }
 }
@@ -221,58 +164,58 @@ void des() {
 }
 void and_op(int val1, int val2) {
     switch (val1) {
-        case 3:
-            A = A & val2;
-            return;
-        case 4:
-            B = B & val2;
-            return;
-        case 5:
-            C = C & val2;
-            return;
+      case 3:
+          A = A & val2;
+          return;
+      case 4:
+          B = B & val2;
+          return;
+      case 5:
+          C = C & val2;
+          return;
     }
 }
 void or_op(int val1, int val2) {
     switch (val1) {
-        case 3:
-            A = A | val2;
-            return;
-        case 4:
-            B = B | val2;
-            return;
-        case 5:
-            C = C | val2;
-            return;
+      case 3:
+          A = A | val2;
+          return;
+      case 4:
+          B = B | val2;
+          return;
+      case 5:
+          C = C | val2;
+          return;
     }
 }
 void not_op(int val1) {
     switch (val1) {
-        case 3:
-            A = ~A;
-            return;
-        case 4:
-            B = ~B;
-            return;
-        case 5:
-            C = ~C;
-            return;
+      case 3:
+          A = ~A;
+          return;
+      case 4:
+          B = ~B;
+          return;
+      case 5:
+          C = ~C;
+          return;
     }
 }
 void xor_op(int val1, int val2) {
     switch (val1) {
-        case 3:
-            A = A ^ val2;
-            return;
-        case 4:
-            B = B ^ val2;
-            return;
-        case 5:
-            C = C ^ val2;
-            return;
+      case 3:
+          A = A ^ val2;
+          return;
+      case 4:
+          B = B ^ val2;
+          return;
+      case 5:
+          C = C ^ val2;
+          return;
     }
 }
 void shr(int val) {
-    switch (val) {
+  switch (val) {
     case 3:
         A = A >> 1;
         return;
@@ -282,10 +225,10 @@ void shr(int val) {
     case 5:
         C = C >> 1;
         return;
-    }
+  }
 }
 void shl(int val) {
-    switch (val) {
+  switch (val) {
     case 3:
         A = A << 1;
         return;
@@ -295,7 +238,7 @@ void shl(int val) {
     case 5:
         C = C << 1;
         return;
-    }
+  }
 }
 
 void PoP(char inp) {
@@ -312,23 +255,37 @@ void PoP(char inp) {
 
 
 void printState() {
-    printf("A: %d, B: %d, C: %d, M: %d, N: %d, Z: %d, PC: %d/%d\n",
-            A, B, C, M, N, Z, PC / 7, PC);
+    Serial.print("A: ");
+    Serial.print(A);
+    Serial.print(", B: ");
+    Serial.print(B);
+    Serial.print(", C: ");
+    Serial.print(C);
+    Serial.print(", M: ");
+    Serial.print(M);
+    Serial.print(", N: ");
+    Serial.print(N);
+    Serial.print(", Z: ");
+    Serial.print(Z);
+    Serial.print(", PC: ");
+    Serial.print(PC / 7);
+    Serial.print("/");
+    Serial.println(PC);
 }
 
 void execute() {
-    while (RUNNING==1) {
-        printState();
-        b0 = PROGMEM[PC + 1];
-        b1 = PROGMEM[PC + 2];
-        b2 = PROGMEM[PC + 3];
-        b3 = PROGMEM[PC + 4];
-        b4 = PROGMEM[PC + 5];
-        b5 = PROGMEM[PC + 6];
-        main_byte = PROGMEM[PC];
+    while (1==1) {
+        //printState();
+        b0 = PROG_MEM[PC + 1];
+        b1 = PROG_MEM[PC + 2];
+        b2 = PROG_MEM[PC + 3];
+        b3 = PROG_MEM[PC + 4];
+        b4 = PROG_MEM[PC + 5];
+        b5 = PROG_MEM[PC + 6];
+        main_byte = PROG_MEM[PC];
         switch (main_byte) {
             case 0:
-                hlt();
+                return;
             case 1:
                 if (b0 == 1 or b0 == 2) {
                     pua(parse_operand(b0, b1, b2));
@@ -389,11 +346,11 @@ void execute() {
                 jmp(parse_operand(b0, b1, b2));
                 break;
             case 12:
-                if (b0 == 1 or b0 == 2 and b1 != 1 or b1 != 2) {
+                if ((b0 == 1 or b0 == 2) && (b1 != 1 or b1 != 2)) {
                     add(parse_operand(b0, b2, b3), parse_operand(b1, b4, 0));
-                } else if (b0 != 1 or b0 != 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 != 1 or b0 != 2) && (b1 == 1 or b1 == 2)) {
                     add(parse_operand(b0, b2, 0), parse_operand(b1, b3, b4));
-                } else if (b0 == 1 or b0 == 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 == 1 or b0 == 2) && (b1 == 1 or b1 == 2)) {
                     add(parse_operand(b0, b2, b3), parse_operand(b1, b4, b5));
                 } else {
                     add(parse_operand(b0, b2, 0), parse_operand(b1, b3, 0));
@@ -401,11 +358,11 @@ void execute() {
                 PC += 7;
                 break;
             case 13:
-                if (b0 == 1 or b0 == 2 and b1 != 1 or b1 != 2) {
+                if ((b0 == 1 or b0 == 2) && (b1 != 1 or b1 != 2)) {
                     sub(parse_operand(b0, b2, b3), parse_operand(b1, b4, 0));
-                } else if (b0 != 1 or b0 != 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 != 1 or b0 != 2) && (b1 == 1 or b1 == 2)) {
                     sub(parse_operand(b0, b2, 0), parse_operand(b1, b3, b4));
-                } else if (b0 == 1 or b0 == 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 == 1 or b0 == 2) && (b1 == 1 or b1 == 2)) {
                     sub(parse_operand(b0, b2, b3), parse_operand(b1, b4, b5));
                 } else {
                     sub(parse_operand(b0, b2, 0), parse_operand(b1, b3, 0));
@@ -413,11 +370,11 @@ void execute() {
                 PC += 7;
                 break;
             case 14:
-                if (b0 == 1 or b0 == 2 and b1 != 1 or b1 != 2) {
+                if ((b0 == 1 or b0 == 2) && (b1 != 1 or b1 != 2)) {
                     mul(parse_operand(b0, b2, b3), parse_operand(b1, b4, 0));
-                } else if (b0 != 1 or b0 != 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 != 1 or b0 != 2) && (b1 == 1 or b1 == 2)) {
                     mul(parse_operand(b0, b2, 0), parse_operand(b1, b3, b4));
-                } else if (b0 == 1 or b0 == 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 == 1 or b0 == 2) && (b1 == 1 or b1 == 2)) {
                     mul(parse_operand(b0, b2, b3), parse_operand(b1, b4, b5));
                 } else {
                     mul(parse_operand(b0, b2, 0), parse_operand(b1, b3, 0));
@@ -425,11 +382,11 @@ void execute() {
                 PC += 7;
                 break;
             case 15:
-                if (b0 == 1 or b0 == 2 and b1 != 1 or b1 != 2) {
+                if ((b0 == 1 or b0 == 2) && (b1 != 1 or b1 != 2)) {
                     DIV(parse_operand(b0, b2, b3), parse_operand(b1, b4, 0));
-                } else if (b0 != 1 or b0 != 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 != 1 or b0 != 2) && (b1 == 1 or b1 == 2)) {
                     DIV(parse_operand(b0, b2, 0), parse_operand(b1, b3, b4));
-                } else if (b0 == 1 or b0 == 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 == 1 or b0 == 2) && (b1 == 1 or b1 == 2)) {
                     DIV(parse_operand(b0, b2, b3), parse_operand(b1, b4, b5));
                 } else {
                     DIV(parse_operand(b0, b2, 0), parse_operand(b1, b3, 0));
@@ -441,7 +398,7 @@ void execute() {
                         PC = parse_operand(b0, b2, b3);
                         stack_append(parse_operand(b0, b2, b3));
                     } else {
-                        PC += 7;
+                    PC += 7;
                     }
                     break;
             case 17:
@@ -449,7 +406,7 @@ void execute() {
                         PC = parse_operand(b0, b2, b3);
                         stack_append(parse_operand(b0, b2, b3));
                     } else {
-                        PC += 7;
+                    PC += 7;
                     }
                     break;
             case 18:
@@ -461,11 +418,11 @@ void execute() {
                 PC += 7;
                 break;
             case 19:
-                if (b0 == 1 or b0 == 2 and b1 != 1 or b1 != 2) {
+                if ((b0 == 1 or b0 == 2) && (b1 != 1 or b1 != 2)) {
                     mem(parse_operand(b0, b2, b3), parse_operand(b1, b4, 0));
-                } else if (b0 != 1 or b0 != 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 != 1 or b0 != 2) && (b1 == 1 or b1 == 2)) {
                     mem(parse_operand(b0, b2, 0), parse_operand(b1, b3, b4));
-                } else if (b0 == 1 or b0 == 2 and b1 == 1 or b1 == 2) {
+                } else if ((b0 == 1 or b0 == 2) && (b1 == 1 or b1 == 2)) {
                     mem(parse_operand(b0, b2, b3), parse_operand(b1, b4, b5));
                 } else {
                     mem(parse_operand(b0, b2, 0), parse_operand(b1, b3, 0));
@@ -485,22 +442,22 @@ void execute() {
                 PC += 7;
                 break;
             case 22:
-            switch (b1) {
-                case 0:
-                    PoP(' ');
-                    break;
-                case 3:
-                    PoP('A');
-                    break;
-                case 4:
-                    PoP('B');
-                    break;
-                case 5:
-                    PoP('C');
-                    break;
-            }
-            PC += 7;
-            break;
+                switch (b1) {
+                    case 0:
+                        PoP(' ');
+                        break;
+                    case 3:
+                        PoP('A');
+                        break;
+                    case 4:
+                        PoP('B');
+                        break;
+                    case 5:
+                        PoP('C');
+                        break;
+                }
+                PC += 7;
+                break;
             case 23:
                 sna();
                 PC += 7;
@@ -549,21 +506,87 @@ void execute() {
                 PC += 7;
                 break;
             default:
-                printf("INVALID INST AT ");
-                printf("%d\n", PC);
+                Serial.print("INVALID INST AT ");
+                Serial.println(PC);
                 while (1==1) {}
         }
-        A = A % 65536;
-        B = B % 65536;
-        C = C % 65536;
+        A &= 0xFFFF;
+        B &= 0xFFFF;
+        C &= 0xFFFF;
     }
 }
 
+uint8_t inst[7] = {0};
 
-int main() {
-    load_PERM_MEM();
-    load_PROGMEM();
-    execute();
-    while (1==1) {}
-    return 0;
+
+void load_PROG_PERM() {
+    File f1 = SD.open("PROGMEM.BIN", FILE_READ);
+    File f2 = SD.open("PERMEM.BIN", FILE_READ);
+
+    if (!f1 || !f2) {
+        if (f1) f1.close();
+        if (f2) f2.close();
+        Serial.println("LOADING FAILED. PLEASE CHECK MICRO SD CARD");
+        while (1) {}  // halt
+    }
+
+    // --- Load PROG_MEM ---
+    size_t totalRead = 0;
+    while (totalRead < sizeof(PROG_MEM)) {
+        size_t r = f1.read(PROG_MEM + totalRead, sizeof(PROG_MEM) - totalRead);
+        if (r == 0) {
+            Serial.println("ERROR: Unexpected EOF while loading PROG_MEM");
+            while (1) {}
+        }
+        totalRead += r;
+    }
+
+    // --- Load PERM_MEM ---
+    totalRead = 0;
+    while (totalRead < sizeof(PERM_MEM)) {
+        size_t r = f2.read(PERM_MEM + totalRead, sizeof(PERM_MEM) - totalRead);
+        if (r == 0) {
+            Serial.println("ERROR: Unexpected EOF while loading PERM_MEM");
+            while (1) {}
+        }
+        totalRead += r;
+    }
+
+    f1.close();
+    f2.close();
+
+    Serial.println("LOADING COMPLETE");
+}
+
+void savePROGMEM(uint16_t addr, uint8_t wchar) {
+    File f = SD.open("PROGMEM.BIN", FILE_WRITE);
+    if (!f) {Serial.printf("PROGMEM.BIN IS MISSING\n"); while (1==1) {}}
+    f.seek(addr);
+    f.write(wchar);
+    f.close();
+}
+
+void savePERMEM(uint16_t addr, uint8_t wchar) {
+    File f = SD.open("PERMEM.BIN", FILE_WRITE);
+    if (!f) {Serial.printf("PERMEM.BIN IS MISSING\n"); while (1==1) {}}
+    f.seek(addr);
+    f.write(wchar);
+    f.close();
+}
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  if (!SD.begin(BUILTIN_SDCARD)) {
+    Serial.println("SD init failed!");
+    return;
+  }
+  load_PROG_PERM();
+  Serial.println("Begining...");
+  execute();
+  Serial.println("Stopped");
+}
+
+void loop() {
+
 }
